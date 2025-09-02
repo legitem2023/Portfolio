7dd168d4062568179d95e5ddcaa42f9f51d64a00
Robert Marquez <legitem2023@gmail.com>
2025-09-02 17:26:05 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // app/page.jsx
 'use client'
-import MainLayout from '@/components/layout/MainLayout';
+import MainLayout from './components/layout/MainLayout';
 
 export default function Home() {
   return <MainLayout />;
