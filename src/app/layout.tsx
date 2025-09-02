2cbab3ee85976e6b441d4e363176e8ed03b212c1
Robert Marquez <legitem2023@gmail.com>
2025-09-02 13:28:28 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "./media1080px.css"
 import "./media600px.css"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+//import { ToastContainer, toast } from 'react-toastify';
+//import 'react-toastify/dist/ReactToastify.css';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,12 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <video autoPlay muted loop id="background-video">
-          <source src="./matrix-digital-background-free-video.mp4" type="video/mp4"/>
-        </video>
         <div className="BackGroundImage"></div>
         {children}
-        <ToastContainer />
         </body>
     </html>
   );
