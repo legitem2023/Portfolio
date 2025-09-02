3dbc60a0166ab0f1b50733b01e8c7e5278497dd9
Robert Marquez <legitem2023@gmail.com>
2025-09-02 13:51:40 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import "./media1080px.css"
-import "./media600px.css"
-//import { ToastContainer, toast } from 'react-toastify';
-//import 'react-toastify/dist/ReactToastify.css';
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Robert Marquez",
   description: "Portfolio",
