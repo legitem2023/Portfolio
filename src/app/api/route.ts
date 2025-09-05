f070c88b350e640204f2e49ea4be2f8f7fecb7b4
Robert Marquez <legitem2023@gmail.com>
2025-09-06 02:20:07 +0800

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,8 @@
-// app/api/graphql/route.ts
 import { createYoga } from "graphql-yoga";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { typeDefs } from "../../graphql/typeDefs";
 import { resolvers } from "../../graphql/resolvers";
 
-// Build schema properly
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
@@ -13,7 +11,8 @@ const schema = makeExecutableSchema({
 const { handleRequest } = createYoga({
   schema,
   graphqlEndpoint: "/api/graphql",
-  fetchAPI: { Response }, // required for Next.js App Router
+  fetchAPI: { Response },
+  graphiql: false, // disable here
 });
 
 export { handleRequest as GET, handleRequest as POST };
