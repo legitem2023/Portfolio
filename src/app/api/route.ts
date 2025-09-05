a13d4d25acf1fc1151e64acfa7c70d4c49e87e58
Robert Marquez <legitem2023@gmail.com>
2025-09-06 01:58:24 +0800

    Create route.ts

---
+++ src/app/api/route.ts
@@ -0,0 +1,15 @@
+// app/api/graphql/route.ts
+import { createYoga } from "graphql-yoga";
+import { typeDefs } from "@/graphql/typeDefs";
+import { resolvers } from "@/graphql/resolvers";
+
+const { handleRequest } = createYoga({
+  schema: {
+    typeDefs,
+    resolvers,
+  },
+  graphqlEndpoint: "/api/graphql",
+  fetchAPI: { Response }, // required in Next.js App Router
+});
+
+export { handleRequest as GET, handleRequest as POST };
