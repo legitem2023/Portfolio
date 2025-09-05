6bacafbf70f699fc2cf2e113c4efc93ff0e9985a
Robert Marquez <legitem2023@gmail.com>
2025-09-06 02:22:18 +0800

    Create route.ts

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,18 @@
+import { createYoga } from "graphql-yoga";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { typeDefs } from "../../../graphql/typeDefs";
+import { resolvers } from "../../../graphql/resolvers";
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const { handleRequest } = createYoga({
+  schema,
+  graphqlEndpoint: "/api/graphql",
+  fetchAPI: { Response },
+  graphiql: false, // disable here
+});
+
+export { handleRequest as GET, handleRequest as POST };
