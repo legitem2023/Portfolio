07e883e3d62706a7a944424533b3063c03e3e759
Robert Marquez <legitem2023@gmail.com>
2025-09-06 00:05:49 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ const EcommercePage: React.FC = () => {
       <DeluxeNavTabs />
       
       {/* Footer */}
-      <footer className="bg-amber-200 text-white pt-16 pb-8 px-4">
+      <footer className="bg-indigo-50 text-white pt-16 pb-8 px-4">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8 mb-12">
             <div>
