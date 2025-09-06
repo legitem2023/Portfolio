11c376b0e5f1ae5b89638b05ecf99f1eb8c68165
Robert Marquez <legitem2023@gmail.com>
2025-09-06 13:51:06 +0800

    Update apollo-client.ts

--- src/app/lib/apollo-client.ts
+++ src/app/lib/apollo-client.ts
@@ -1,25 +1,31 @@
-import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
-import { onError } from '@apollo/client/link/error';
+import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
+  if (graphQLErrors) {
     graphQLErrors.forEach(({ message, locations, path }) =>
       console.error(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-      ),
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
     );
+  }
   if (networkError) console.error(`[Network error]: ${networkError}`);
 });
 
+const isServer = typeof window === "undefined";
+
 const httpLink = new HttpLink({
-  uri: "/api/graphql",
-  credentials: 'include',
+  uri: isServer
+    ? process.env.NEXT_PUBLIC_GRAPHQL_URL ??
+      "https://portfolio-xi-eight-92.vercel.app/api/graphql"
+    : "/api/graphql",
+  credentials: "include",
 });
 
 export function getServerClient() {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: from([errorLink, httpLink]),
-    ssrMode: true, // This is important for server-side
+    ssrMode: true, // ✅ important for SSR
   });
 }
