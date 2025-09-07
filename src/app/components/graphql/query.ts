3449eb168ba219143e30832e24138a838955acfa
Robert Marquez <legitem2023@gmail.com>
2025-09-07 13:56:41 +0800

    Update query.ts

--- src/app/components/graphql/query.ts
+++ src/app/components/graphql/query.ts
@@ -1,5 +1,32 @@
 import { gql } from '@apollo/client';
 
+
+export const MANAGEMENTPRODUCTS = gql`
+query GetProducts {
+  getProducts {
+        id
+        name
+        description
+        price
+        salePrice
+        sku
+        stock
+        images
+        category {
+            id    
+        }
+        brand
+        weight
+        dimensions
+        isActive
+        featured
+        tags
+        createdAt
+        updatedAt
+  }
+}
+`
+
 export const GETCATEGORY = gql`
 query GetCategories {
   categories{
