68204622599e566a54d7d4c5ff93204bbfad5ac3
Robert Marquez <legitem2023@gmail.com>
2025-09-07 22:18:05 +0800

    Update mutation.ts

--- src/app/components/graphql/mutation.ts
+++ src/app/components/graphql/mutation.ts
@@ -1,6 +1,20 @@
 import { gql } from "@apollo/client";
 
-
+export const LOGIN = gql`
+mutation Login($input: LoginInput) {
+  login(input: $input) {
+    statusText
+    token
+  }
+}`
+export const FBLOGIN = gql`
+mutation LoginWithFacebook($input: GoogleLoginInput!) {
+  loginWithFacebook(input: $input) {
+    token
+    statusText
+  }
+}
+`
 export const INSERTPRODUCT = gql`
   mutation InsertProduct( $id: String, $name: String!, $description: String!, $price: Float!, $salePrice: Float!, $sku: String! ) {
     createProduct(id: $id, name: $name, description: $description, price: $price, salePrice: $salePrice, sku: $sku) {
