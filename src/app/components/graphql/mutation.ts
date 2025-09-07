36ff86f2f7780a46ddf210887918ce0097c711b7
Robert Marquez <legitem2023@gmail.com>
2025-09-07 14:56:26 +0800

    Update mutation.ts

--- src/app/components/graphql/mutation.ts
+++ src/app/components/graphql/mutation.ts
@@ -1,5 +1,14 @@
 import { gql } from "@apollo/client";
 
+
+export const INSERTPRODUCT = gql`
+  mutation InsertProduct( $id: String, $name: String!, $description: String!, $price: Float!, $salePrice: Float!, $sku: String ) {
+    createProduct(id: $id, name: $name, description: $description, price: $price, salePrice: $salePrice, sku: $sku) {
+      statusText
+    }  
+  }
+`
+
 export const INSERTCATEGORY = gql`
   mutation InsertCategory($name: String!, $description: String!, $status: Boolean!) {
     createCategory(description: $description, name: $name, status: $status) {
