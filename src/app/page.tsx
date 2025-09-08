721891ab0b9b8f411fca77b464682d94090ec9d5
Robert Marquez <legitem2023@gmail.com>
2025-09-08 16:41:29 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import DeluxeNavTabs from './components/DeluxeNavTabs';
 import Footer from './components/Footer';
-
+import Header from './components/Header';
 import Image from 'next/image';
 import { 
   Phone,
@@ -16,16 +16,7 @@ import {
 const EcommercePage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-indigo-50 to-violet-50 p-0">
-      <div className="text-center p-4">
-        
-        <Image
-              src={`/Dlogo.svg`}
-              alt="home1"
-              height="80"
-              width="160"
-              className="h-25 w-50"
-            />
-      </div>
+      <Header/>
       <DeluxeNavTabs />
       
       {/* Footer */}
