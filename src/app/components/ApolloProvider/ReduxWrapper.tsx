e52ba0f972b1d53e38faf5ed689e8a1d34ff140e
Robert Marquez <legitem2023@gmail.com>
2025-09-13 19:08:29 +0800

    Update ReduxWrapper.tsx

--- src/app/components/ApolloProvider/ReduxWrapper.tsx
+++ src/app/components/ApolloProvider/ReduxWrapper.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../../../../Redux/store'; // Import both store and persistor
 import React from 'react'
-import { Apollo } from './Apollo';
 
 export default function ReduxWrapper({
   children,
@@ -13,7 +12,7 @@ export default function ReduxWrapper({
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Apollo>{children}</Apollo>
+        {children}
       </PersistGate>
     </Provider>
   );
