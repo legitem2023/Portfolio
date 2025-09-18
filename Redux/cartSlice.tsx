becec7264ea4241ee4877d19ad221ff4029e44f2
Robert Marquez <legitem2023@gmail.com>
2025-09-18 09:43:02 +0800

    Create cartSlice.tsx

---
+++ Redux/cartSlice.tsx
@@ -0,0 +1,55 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CartItem } from "../types/types";
+
+interface CartState {
+  cartItems: CartItem[];
+}
+
+const initialState: CartState = {
+  cartItems: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<CartItem>) => {
+      const existingItem = state.cartItems.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.cartItems.push({ ...action.payload, quantity: 1 });
+      }
+    },
+
+    removeFromCart: (state, action: PayloadAction<{ id: string }>) => {
+      state.cartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+    },
+
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+
+    changeQuantity: (
+      state,
+      action: PayloadAction<{ id: string; quantity: number }>
+    ) => {
+      const { id, quantity } = action.payload;
+      const item = state.cartItems.find((item) => item.id === id);
+
+      if (item && quantity > 0) {
+        item.quantity = quantity;
+      }
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, clearCart, changeQuantity } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
