c2158841f85c23e79f140bef02f5941dfc0d1bf9
Robert Marquez <legitem2023@gmail.com>
2025-09-21 23:11:28 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import ReduxWrapper from "./components/ApolloProvider/ReduxWrapper"; 
 import { ApolloWrapper } from './components/ApolloWrapper';
+// ✅ Toastify import
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Inter } from "next/font/google";
 import LoadEruda from "./LoadEruda";
 import "./globals.css";
@@ -21,6 +24,7 @@ export default function RootLayout({
         <LoadEruda/>
         <div className="BackGroundImage"></div>
         <ReduxWrapper>
+          <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop />
           <ApolloWrapper>{children}</ApolloWrapper>
         </ReduxWrapper>
       </body>
