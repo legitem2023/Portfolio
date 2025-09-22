5d8585a405303c5cbbc8c03a875fedff6c080c35
Robert Marquez <legitem2023@gmail.com>
2025-09-22 16:41:57 +0800

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -53,9 +53,9 @@ const Header: React.FC = () => {
     <div className="relative bg-gradient-to-r from-violet-100 to-indigo-100 bg-opacity-90 backdrop-blur-sm shadow-sm p-2 aspect-[4/1] sm:aspect-[9/1]">
       
         <AnimatedCrowd/>
-      <div className="z-20 flex items-center justify-between p-2 bg-rose-500 h-[100%] w-[100%]">
+      <div className="z-20 flex items-center justify-between p-2 h-[100%] w-[100%]">
       
-        <div className="z-20 h-[100%] flex items-center bg-rose-500">
+        <div className="z-20 h-[100%] flex items-center">
           <Image
             src="/Dlogo.svg"
             alt="Logo"
@@ -66,7 +66,7 @@ const Header: React.FC = () => {
         </div>
            
     
-        <div className="z-20 h-[100%] flex items-center bg-rose-500" ref={dropdownRef}>
+        <div className="z-20 h-[100%] flex items-center" ref={dropdownRef}>
           
           <button
             onClick={() => setIsDropdownOpen(!isDropdownOpen)}
