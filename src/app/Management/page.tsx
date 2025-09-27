85be744903a4b066a881dea142bb0682e3c631c9
Robert Marquez <legitem2023@gmail.com>
2025-09-27 20:34:07 +0800

    Update page.tsx

--- src/app/Management/page.tsx
+++ src/app/Management/page.tsx
@@ -109,7 +109,7 @@ console.log(productData);
     const category = categories.find(c => c.id === parseInt(newProduct.categoryId));
     
     const product: Product = {
-      id: products.id,
+      id: newProduct.id,
       name: newProduct.name,
       variants: newProduct.variants,
       description: newProduct.description,
