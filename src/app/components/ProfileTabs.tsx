8d780ee74075c3ee9c31b2cdada34484a0255787
Robert Marquez <legitem2023@gmail.com>
2025-10-02 07:57:10 +0800

    Update ProfileTabs.tsx

--- src/app/components/ProfileTabs.tsx
+++ src/app/components/ProfileTabs.tsx
@@ -12,6 +12,12 @@ const ProfileTabs = ({ activeTab, onTabChange, tabsConfig }: ProfileTabsProps) =
     const iconClass = "w-4 h-4 md:w-5 md:h-5 mr-2";
     
     switch (iconName) {
+      case 'address':
+        return (
+          <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+          </svg>
+        );
       case 'user':
         return (
           <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
@@ -31,12 +37,7 @@ const ProfileTabs = ({ activeTab, onTabChange, tabsConfig }: ProfileTabsProps) =
             <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
           </svg>
         );
-      case 'address':
-        return (
-          <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
-          </svg>
-        );
+
       default:
         return null;
     }
