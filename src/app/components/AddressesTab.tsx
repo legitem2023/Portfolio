22a190cfbf8828114fc318cb2f17257de1d34c45
Robert Marquez <legitem2023@gmail.com>
2025-10-02 22:33:23 +0800

    Update AddressesTab.tsx

--- src/app/components/AddressesTab.tsx
+++ src/app/components/AddressesTab.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from 'react';
 import { gql, useMutation } from '@apollo/client';
 import AddressForm from './Addresses/AddressForm';
 // Mutation for setting default address
