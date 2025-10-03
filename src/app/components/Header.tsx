e83b96611b53cae51384cf6a45a8da7f524bb6e6
Robert Marquez <legitem2023@gmail.com>
2025-10-03 23:50:13 +0800

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -132,7 +132,10 @@ const Header: React.FC = () => {
               </div>
               <div
                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50 hover:text-blue-600"
-                onClick={() => setIsDropdownOpen(false)}
+                onClick={() => {
+                  setIsDropdownOpen(false);
+                  dispatch(setActiveIndex(10));
+                }}
               >
                 <i className="fas fa-shopping-bag mr-2 text-gray-400"></i>Orders
               </div>
