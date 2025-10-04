55734f3623c02f72c344401eff60ce0300b4ccf6
Robert Marquez <legitem2023@gmail.com>
2025-10-04 17:55:49 +0800

    Update UserProfileShimmerRed.tsx

--- src/app/components/UserProfileShimmerRed.tsx
+++ src/app/components/UserProfileShimmerRed.tsx
@@ -6,7 +6,7 @@ const UserProfileShimmerRed = () => {
     <div className="bg-red-50 min-h-screen max-w-2xl">
       {/* Cover Photo Shimmer with red theme */}
       <div className="h-48 md:h-60 lg:h-80 bg-gradient-to-r from-red-100 to-red-200 animate-pulse relative">
-        <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-red-300/20 to-transparent"></div>
+        <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-red-300/20 to-transparent"></div>
         
         {/* Profile Picture Shimmer */}
         <div className="absolute -bottom-12 md:-bottom-16 left-4 md:left-8 transform md:transform-none">
