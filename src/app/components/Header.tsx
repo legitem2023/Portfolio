4e89f5337c98b0ba19c233477335c8272ec6f5c9
Robert Marquez <legitem2023@gmail.com>
2025-10-05 21:07:20 +0800

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -61,7 +61,7 @@ const Header: React.FC = () => {
 
   return (
     <div className="relative bg-gradient-to-r from-violet-100 to-indigo-100 bg-opacity-90 backdrop-blur-sm shadow-sm p-2 aspect-[4/1] sm:aspect-[9/1]">
-      <Ads/>
+      
       {/* <AdDrawer
         isOpen={drawer.isOpen}
         onClose={drawer.close}
@@ -142,7 +142,7 @@ const Header: React.FC = () => {
 
       </div>
       
-  
+  <Ads/>
     </div>
   );
 };
