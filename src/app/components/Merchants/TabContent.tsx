af4e4c66e50dc5d5d4dbb563bf5b5bd44089efb1
Robert Marquez <legitem2023@gmail.com>
2025-10-12 15:56:38 +0800

    Update TabContent.tsx

--- src/app/components/Merchants/TabContent.tsx
+++ src/app/components/Merchants/TabContent.tsx
@@ -62,28 +62,7 @@ const TabContent = ({ activeTab, user, userId, refetch }: TabContentProps) => {
       )}
     </div>
   );
-
-  const renderPhotosTab = () => (
-    <div className="max-w-4xl mx-auto px-4 py-6">
-      <div className="text-center py-12 text-gray-500">
-        <svg className="w-16 h-16 mx-auto text-gray-300" fill="currentColor" viewBox="0 0 20 20">
-          <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
-        </svg>
-        <p className="mt-4 text-lg">Photos content coming soon</p>
-      </div>
-    </div>
-  );
-
-  const renderFriendsTab = () => (
-    <div className="max-w-4xl mx-auto px-4 py-6">
-      <div className="text-center py-12 text-gray-500">
-        <svg className="w-16 h-16 mx-auto text-gray-300" fill="currentColor" viewBox="0 0 20 20">
-          <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
-        </svg>
-        <p className="mt-4 text-lg">Friends content coming soon</p>
-      </div>
-    </div>
-  );
+  
 
   const handleAddressUpdate = async () => {
     // Refetch user data to get updated addresses
@@ -98,10 +77,6 @@ const TabContent = ({ activeTab, user, userId, refetch }: TabContentProps) => {
   switch (activeTab) {
     case 'posts':
       return renderPostsTab();
-    case 'photos':
-      return renderPhotosTab();
-    case 'friends':
-      return renderFriendsTab();
     case 'address':
       return renderAddressTab();
     default:
