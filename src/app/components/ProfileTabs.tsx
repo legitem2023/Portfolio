dec89894457ff2cc556c3654dd9550c5a50102ef
Robert Marquez <legitem2023@gmail.com>
2025-10-13 22:55:57 +0800

    Update ProfileTabs.tsx

--- src/app/components/ProfileTabs.tsx
+++ src/app/components/ProfileTabs.tsx
@@ -10,30 +10,29 @@ interface ProfileTabsProps {
 const ProfileTabs = ({ activeTab, onTabChange, tabsConfig }: ProfileTabsProps) => {
   const getTabIcon = (iconName: string) => {
     const iconClass = "w-4 h-4 md:w-5 md:h-5 mr-2";
-    
-    switch (iconName) {
-      case 'address':
-        return (
-          <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
-          </svg>
-        );
-      case 'user':
-        return (
-          <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 005 10a6 6 0 0012 0c0-1.003-.21-1.96-.59-2.808A5 5 0 0010 11z" clipRule="evenodd" />
-          </svg>
-        );
-      case 'product':
-        return (
-          <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 005 10a6 6 0 0012 0c0-1.003-.21-1.96-.59-2.808A5 5 0 0010 11z" clipRule="evenodd" />
-          </svg>
-        );
-      default:
-        return null;
-    }
-  };
+
+  switch (iconName) {
+  case 'address':
+    return (
+      <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
+        <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+      </svg>
+    );
+  case 'user':
+    return (
+      <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
+        <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+      </svg>
+    );
+  case 'product':
+    return (
+      <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
+        <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
+      </svg>
+    );
+  default:
+    return null;
+}
 
   return (
     <div className="mt-6 border-t border-gray-300 flex overflow-x-auto scrollbar-hide">
