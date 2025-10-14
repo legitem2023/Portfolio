760d4aa043f0f71dd89c60f7ebc7f125b9d85e4a
Robert Marquez <legitem2023@gmail.com>
2025-10-14 09:17:17 +0800

    Update DeluxeNavTabs.tsx

--- src/app/components/DeluxeNavTabs.tsx
+++ src/app/components/DeluxeNavTabs.tsx
@@ -14,6 +14,7 @@ import {
   ShoppingBag,
   Target,
   Star,
+  Store,
   Gift,
   Shirt,
   Briefcase,
@@ -104,8 +105,8 @@ const DeluxeNavTabs: React.FC = () => {
     },
     {
       id:4,
-      label: 'Luxe Brands',
-      icon: <Star size={18} />,
+      label: 'Merchants',
+      icon: <Store size={18} />,
       content: (
         <MerchantsPage/>
       ),
