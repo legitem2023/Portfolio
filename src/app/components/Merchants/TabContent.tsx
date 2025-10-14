4c5a6f3aed840cc0891e30b15f9bfbff3ca489a4
Robert Marquez <legitem2023@gmail.com>
2025-10-14 14:41:26 +0800

    Update TabContent.tsx

--- src/app/components/Merchants/TabContent.tsx
+++ src/app/components/Merchants/TabContent.tsx
@@ -72,7 +72,7 @@ const TabContent = ({ activeTab, user, userId, refetch }: TabContentProps) => {
   };
   
   const renderProductTab = () => (
-      <div className="max-w-4xl mx-auto p-0">
+      <div className="max-w-4xl mx-auto p-4">
           <ProductThumbnails products={user?.products} />
        </div>
       );
