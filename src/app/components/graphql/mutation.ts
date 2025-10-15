26e534354d09d22852ab0a1937f47d8778a806b7
Robert Marquez <legitem2023@gmail.com>
2025-10-15 23:43:28 +0800

    Update mutation.ts

--- src/app/components/graphql/mutation.ts
+++ src/app/components/graphql/mutation.ts
@@ -57,8 +57,8 @@ export const FBLOGIN = gql`
 `;
 
 export const INSERTPRODUCT = gql`
-  mutation InsertProduct($id: String, $name: String, $description: String, color: String, size: String, $price: Float, $salePrice: Float, $sku: String, $supplierId: String) {
-    createProduct(id: $id, name: $name, description: $description, color: $color, size: $size, price: $price, salePrice: $salePrice, sku: $sku, supplierId: $supplierId) {
+  mutation InsertProduct($id: String, $name: String, $description: String, color: String, stock: Int, size: String, $price: Float, $salePrice: Float, $sku: String, $supplierId: String) {
+    createProduct(id: $id, name: $name, description: $description, color: $color, stock: $stock, size: $size, price: $price, salePrice: $salePrice, sku: $sku, supplierId: $supplierId) {
       statusText
     }  
   }
