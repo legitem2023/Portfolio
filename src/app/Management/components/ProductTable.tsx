6388b80bdf9572bcf2e5c925e2a85c3e4232240b
Robert Marquez <legitem2023@gmail.com>
2025-10-16 21:59:26 +0800

    Update image source to use product variants

--- src/app/Management/components/ProductTable.tsx
+++ src/app/Management/components/ProductTable.tsx
@@ -173,9 +173,9 @@ function TableRow({
       <td className="px-6 py-4 whitespace-nowrap">
         <div className="flex items-center">
           <div className="flex-shrink-0 h-10 w-10 bg-gray-300 rounded-md flex items-center justify-center relative overflow-hidden">
-            {product.images && product.images.length > 0 ? (
+            {product.variants.images && product.variants.images.length > 0 ? (
               <img 
-                src={product.images[0]} 
+                src={product.variants.images[0]} 
                 alt={product.name}
                 className="h-full w-full object-cover"
               />
