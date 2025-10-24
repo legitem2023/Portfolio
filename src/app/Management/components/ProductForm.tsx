58165227cd1ce10cd379b33adb58803318be1e37
Robert Marquez <legitem2023@gmail.com>
2025-10-24 21:48:40 +0800

    Update ProductForm.tsx

--- src/app/Management/components/ProductForm.tsx
+++ src/app/Management/components/ProductForm.tsx
@@ -115,7 +115,7 @@ export default function ProductForm({
         ></textarea>
       </div>
       
-      <div className="grid grid-cols-2 gap-4 mb-4">
+      <div className="grid grid-cols-1 gap-4 mb-4">
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-1">Price ($)</label>
           <input
