7d1f3bd3bb71b9a74a9a9e08f8a1ab43ce333931
Robert Marquez <legitem2023@gmail.com>
2025-11-20 22:03:05 +0800

    Update DeluxeNavTabs.tsx

--- src/app/components/DeluxeNavTabs.tsx
+++ src/app/components/DeluxeNavTabs.tsx
@@ -190,8 +190,7 @@ const DeluxeNavTabs: React.FC = () => {
 
   return (
     <div className="w-full mx-auto font-sans z-10">
-      
-      <div className="w-full flex justify-between md:justify-center overflow-x-auto hide-scrollbar mb-1 bg-gradient-to-t from-violet-50 to-white z-20">
+      <div className="fixed bottom-0 left-0 right-0 w-full flex justify-between md:justify-center overflow-x-auto hide-scrollbar mb-0 bg-gradient-to-t from-violet-50 to-white z-50 border-t border-gray-200">
         {tabs.slice(0,6).map((tab) => (
           <button
             key={tab.id}
