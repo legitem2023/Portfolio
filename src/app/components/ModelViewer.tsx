42c813cd229c1e069997d677b943ae01c3c962de
Robert Marquez <legitem2023@gmail.com>
2025-11-21 08:42:11 +0800

    Update ModelViewer.tsx

--- src/app/components/ModelViewer.tsx
+++ src/app/components/ModelViewer.tsx
@@ -1,16 +1,11 @@
 import Script from 'next/script';
-import React, { useState, useEffect } from 'react';
-
-interface ModelData {
-  model: string;
-}
+import React from 'react';
 
 interface ModelViewerProps {
   data: string;
 }
 
 const ModelViewer: React.FC<ModelViewerProps> = ({ data }) => {
-
     return (
         <div className="canvas">
             <Script
@@ -30,7 +25,7 @@ const ModelViewer: React.FC<ModelViewerProps> = ({ data }) => {
                 skybox-image="https://modelviewer.dev/shared-assets/environments/whipple_creek_regional_park_1k_HDR.jpg"
                 skybox-height="2m"
                 max-camera-orbit="auto 90deg auto"
-                style={{ width: 400px, height: 400px , backgroundColor:'red'}}
+                style={{ width: '400px', height: '400px', backgroundColor: 'red' }}
             />
         </div>
     );
