8410f91e8ca2fe0973b403495ff4f23238bdb38c
Robert Marquez <legitem2023@gmail.com>
2025-11-27 20:52:52 +0800

    Update ProductsTab.tsx

--- src/app/Management/components/ProductsTab.tsx
+++ src/app/Management/components/ProductsTab.tsx
@@ -1,7 +1,7 @@
 import { Product, Category, NewProduct } from '../types/types';
 import ProductTable from './ProductTable';
 import ProductForm from './ProductForm';
-
+import Tabs from './Tabs';
 interface ProductsTabProps {
   supplierId: String;
   products: Product[];
@@ -19,15 +19,11 @@ export default function ProductsTab({
   setNewProduct,
   handleProductSubmit
 }: ProductsTabProps) {
-  return (
-    <div className="p-6">
-      <div className="flex justify-between items-center mb-6">
-        <h2 className="text-2xl font-bold text-gray-800">Product Management</h2>
-      </div>
-
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-        
-
+  const tabs = [
+    {
+      id: 'Form',
+      label: 'Form',
+      content: (
         <div className="bg-white shadow-md rounded-lg p-6">
           <h3 className="text-lg font-medium text-gray-800 mb-4">Add New Product</h3>
           <ProductForm
@@ -40,9 +36,30 @@ export default function ProductsTab({
             }}
           />
         </div>
+      ),
+    },
+    {
+      id: 'Data',
+      label: 'Data',
+      content: (
         <div className="lg:col-span-2">
           <ProductTable products={products} />
         </div>
+      ),
+    }
+    ]
+  
+  return (
+    <div className="p-6">
+      <div className="flex justify-between items-center mb-6">
+        <h2 className="text-2xl font-bold text-gray-800">Product Management</h2>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <Tabs/>
+
+
+ 
       </div>
     </div>
   );
