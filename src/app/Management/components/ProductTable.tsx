f3071ae3e17abe7a0758d29068fb665185b1ffec
Robert Marquez <legitem2023@gmail.com>
2025-11-28 23:07:16 +0800

    Update ProductTable.tsx

--- src/app/Management/components/ProductTable.tsx
+++ src/app/Management/components/ProductTable.tsx
@@ -671,7 +671,7 @@ function VariantsModal({
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
                 <p className="mt-4 text-lg font-medium text-gray-900">No variants available</p>
-                <p className="mt-2 text-sm">Click "Add New Variant" to create your first variant</p>
+                <p className="mt-2 text-sm">Click Add New Variant to create your first variant</p>
               </div>
             )}
           </div>
