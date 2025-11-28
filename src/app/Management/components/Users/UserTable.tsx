278a800b5bbddb803e91dcc8b6aa8b59025727e3
Robert Marquez <legitem2023@gmail.com>
2025-11-28 23:39:46 +0800

    Update UserTable.tsx

--- src/app/Management/components/Users/UserTable.tsx
+++ src/app/Management/components/Users/UserTable.tsx
@@ -12,7 +12,7 @@ const UserTable: React.FC<UserTableProps> = ({ users, onViewAddresses }) => {
     <div className="bg-white rounded-lg shadow-md overflow-hidden">
       <div className="overflow-x-auto">
         <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
+          <thead className="bg-gray-800 text-gray-300">
             <tr>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 User
