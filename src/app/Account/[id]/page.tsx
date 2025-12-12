654ddf755d083d67839026c7c9882175f5f02999
Robert Marquez <legitem2023@gmail.com>
2025-12-12 23:03:42 +0800

    Rename EcommercePage to AccountPage and update usage

--- src/app/Account/[id]/page.tsx
+++ src/app/Account/[id]/page.tsx
@@ -4,6 +4,10 @@ import DeluxeNavTabs from '../../components/DeluxeNavTabs';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import Image from 'next/image';
+import { useParams } from 'next/navigation';
+import UserProfile from '../../components/UserProfile';
+import MerchantDetails from '../../components/Merchants/MerchantDetails';
+
 import { 
   Phone,
   Mail,
@@ -13,14 +17,17 @@ import {
   Twitter,
   Youtube
 } from 'lucide-react';
-const EcommercePage: React.FC = () => {
+const AccountPage: React.FC = () => {
+  const params = useParams();
+  const id = params.id;
   return (
     <div className="min-h-screen bg-gradient-to-b from-indigo-50 to-violet-50 p-0">
       <Header/>    
       {/* Footer */}
+      <MerchantDetails userId={id}/>
    <Footer/>
     </div>
   );
 };
 
-export default EcommercePage;
+export default AccountPage;
