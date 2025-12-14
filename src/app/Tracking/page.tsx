ee2be5e4b7b0de8632fceb6aeaba5bfe04fa3430
Robert Marquez <legitem2023@gmail.com>
2025-12-14 12:47:10 +0800

    Create page.tsx

---
+++ src/app/Tracking/page.tsx
@@ -0,0 +1,28 @@
+// pages/index.tsx (or wherever you want to use the component)
+import React from 'react';
+import DeluxeNavTabs from '../components/DeluxeNavTabs';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Image from 'next/image';
+import { 
+  Phone,
+  Mail,
+  MapPin,
+  Facebook,
+  Instagram,
+  Twitter,
+  Youtube
+} from 'lucide-react';
+const EcommercePage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-indigo-50 to-violet-50 p-0">
+      <Header/>
+      <DeluxeNavTabs />
+      
+      {/* Footer */}
+   <Footer/>
+    </div>
+  );
+};
+
+export default EcommercePage;
