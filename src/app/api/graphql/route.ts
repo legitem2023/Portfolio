19b8994fb3ba75e99f613454728d5df804cb0408
Robert Marquez <legitem2023@gmail.com>
2025-12-15 11:28:19 +0800

    Update route.ts

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,4 +1,4 @@
-import { createYoga } from "graphql-yoga";
+/*import { createYoga } from "graphql-yoga";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { typeDefs } from "../../../graphql/schema";
 import { resolvers } from "../../../graphql/resolver";
@@ -13,6 +13,29 @@ const { handleRequest } = createYoga({
   graphqlEndpoint: "/api/graphql",
   fetchAPI: { Response },
   graphiql: true, // disable here
+});
+
+export { handleRequest as GET, handleRequest as POST };*/
+import { createYoga } from "graphql-yoga";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { GraphQLUpload } from "graphql-upload"; // Add this import
+import { typeDefs } from "../../../graphql/schema";
+import { resolvers } from "../../../graphql/resolver";
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers: {
+    ...resolvers,
+    Upload: GraphQLUpload, // Just add this one line
+  },
+});
+
+const { handleRequest } = createYoga({
+  schema,
+  graphqlEndpoint: "/api/graphql",
+  fetchAPI: { Response },
+  graphiql: true,
+  multipart: true, // Add this to enable file uploads
 });
 
 export { handleRequest as GET, handleRequest as POST };
