23ea834b28e3f0d179b91f3b9df41f4f404cebb7
Robert Marquez <legitem2023@gmail.com>
2025-12-16 10:11:10 +0800

    Update StatusBadge.tsx

--- src/app/Management/components/Products/StatusBadge.tsx
+++ src/app/Management/components/Products/StatusBadge.tsx
@@ -1,11 +1,11 @@
 interface StatusBadgeProps {
-  status: string;
+  status: boolean;
 }
 
 export default function StatusBadge({ status }: StatusBadgeProps) {
   return (
     <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-      status === 'Active' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'
+      status === true ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'
     }`}>
       {status}
     </span>
