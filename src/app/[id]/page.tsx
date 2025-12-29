a10745aa576466e436c5c5a4092023bb13cf15fc
Robert Marquez <legitem2023@gmail.com>
2025-12-29 22:44:54 +0800

    Update page.tsx

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,1 +1,27 @@
 
+// pages/index.tsx (or wherever you want to use the component)
+import React from 'react';
+
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Image from 'next/image';
+import { 
+  Phone,
+  Mail,
+  MapPin,
+  Facebook,
+  Instagram,
+  Twitter,
+  Youtube
+} from 'lucide-react';
+const EcommercePage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-indigo-50 to-violet-50 p-0">
+      <Header/>
+      {/* Footer */}
+      <Footer/>
+    </div>
+  );
+};
+
+export default EcommercePage;
