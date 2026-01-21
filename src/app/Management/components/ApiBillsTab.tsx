f189d4dcbe4549e6b7e16ab48d20cad6b43d9003
Robert Marquez <legitem2023@gmail.com>
2026-01-22 00:59:53 +0800

    Update ApiBillsTab.tsx

--- src/app/Management/components/ApiBillsTab.tsx
+++ src/app/Management/components/ApiBillsTab.tsx
@@ -1,14 +1,26 @@
 
 import ApiBillsComponent from './ApiBillsComponent';
+import CreateApiBillForm from './CreateApiBillForm';
+import { useState } from 'react';
 import Tabs from './Tabs';
 export default function ApiBillsTab() {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleSuccess = (createdBill: any) => {
+    console.log('Bill created:', createdBill);
+    // You could update a list of bills here
+  };
   const tabs = [
     {
       id: 'Form',
       label: 'Form',
       content: (
         <div className="bg-white shadow-md rounded-lg p-6">
-          
+          <CreateApiBillForm
+            onSuccess={handleSuccess}
+            onCancel={() => setShowForm(false)}
+            defaultService="AWS"
+          />
         </div>
       ),
     },
