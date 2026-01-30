abe5ce3e059c844eb08d4af7a7c3ff1a84bffb33
Robert Marquez <legitem2023@gmail.com>
2026-01-30 21:21:30 +0800

    Create page.tsx

---
+++ src/app/Rider/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+export default function RiderDashboard() {
+  
+
+
+
+
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Head>
+        <title>VendorCity Rider Dashboard</title>
+        <meta name="description" content="VendorCity Rider Dashboard" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+    </div>
+  );
+      }
