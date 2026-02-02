44491edd147e3b10030788d64584715487441499
Robert Marquez <legitem2023@gmail.com>
2026-02-02 16:32:16 +0800

    Update Seo.ts

--- src/app/components/Seo/Seo.ts
+++ src/app/components/Seo/Seo.ts
@@ -3,8 +3,7 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'VC -  VendorCity | Shop Smarter with VendorCity.net',
-  description:
-    'Discover a smarter way to shop online with VendorCity — your trusted VendorCity. Explore top brands, unbeatable deals, and fast checkout on VendorCity.net',
+  description:'Discover a smarter way to shop online with VendorCity — your trusted VendorCity. Explore top brands, unbeatable deals, and fast checkout on VendorCity.net',
   manifest: '/manifest.json',
   keywords: [
     'VendorCity',
@@ -34,28 +33,26 @@ export const metadata: Metadata = {
 
   openGraph: {
     type: 'website',
-    title: 'DVN - Digital Vendors Network | Shop Smarter with DVN.com',
-    description:
-      'DVN.com brings you closer to thousands of trusted digital vendors. Shop, compare, and buy online — all in one smart network.',
-    url: 'https://dvn.com',
-    siteName: 'DVN',
+    title: 'VC -  VendorCity | Shop Smarter with VendorCity.net',
+    description:'Discover a smarter way to shop online with VendorCity — your trusted VendorCity. Explore top brands, unbeatable deals, and fast checkout on VendorCity.net',
+    url: 'https://VendorCity.net',
+    siteName: 'VendorCity',
     images: [
       {
         url: '/og-image.jpg',
         width: 1200,
         height: 630,
-        alt: 'DVN - Digital Vendors Network | Online Shopping Platform',
+        alt: 'VC -  VendorCity | Shop Smarter with VendorCity.net',
       },
     ],
   },
 
   twitter: {
     card: 'summary_large_image',
-    title: 'DVN - Digital Vendors Network | Shop Smarter with DVN.com',
-    description:
-      'Shop with DVN.com — your digital marketplace for top brands and trusted online vendors. Fast, easy, and secure shopping experience.',
+    title: 'VC -  VendorCity | Shop Smarter with VendorCity.net',
+    description:'Discover a smarter way to shop online with VendorCity — your trusted VendorCity. Explore top brands, unbeatable deals, and fast checkout on VendorCity.net',
     images: ['/og-image.jpg'],
-    creator: '@dvn_official',
+    creator: '@robertmarquez',
   },
 };
 
