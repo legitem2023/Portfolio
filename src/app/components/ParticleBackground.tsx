7e329deb650e686a1f7bff59da24b384d6b2c53b
Robert Marquez <legitem2023@gmail.com>
2026-02-09 10:14:19 +0800

    Update ParticleBackground.tsx

--- src/app/components/ParticleBackground.tsx
+++ src/app/components/ParticleBackground.tsx
@@ -316,7 +316,7 @@ const ParticleBackground: React.FC<ParticleBackgroundProps> = ({
         left: 0,
         width: '100%',
         height: '100%',
-        zIndex: -1,
+        zIndex: 0,
         pointerEvents: enableMouseInteraction ? 'auto' : 'none',
         backgroundColor: backgroundColor
       }}
