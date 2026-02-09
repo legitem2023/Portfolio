cfda3483be2966798a6b7c5c8443b9bd4c5c854f
Robert Marquez <legitem2023@gmail.com>
2026-02-09 23:06:52 +0800

    Update UserProfile.tsx

--- src/app/components/UserProfile.tsx
+++ src/app/components/UserProfile.tsx
@@ -31,12 +31,11 @@ const UserProfile = ({ userId }: { userId: string }) => {
     <div className="min-h-screen p-0">
       <div className="max-w-2xl mx-auto">      {/* Cover Photo */}
       <div className="h-36 relative overflow">
-        <div className="absolute bottom-0 w-full h-1/2">
+        <div className="absolute bottom-0 w-full h-1/2"></div>
         <ParticleBackground/>
-        </div>
-        
         {/* Profile Picture */}
         <div className="absolute -bottom-12 md:-bottom-16 left-4 md:left-8 transform md:transform-none">
+          
           <div className="relative w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40 rounded-full overflow-hidden border-4 border-white shadow-lg">
             <Image
               src={user.avatar || '/NoImage.webp'}
