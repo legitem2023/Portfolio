af62e0c7f0f624910c7ffd6a618ffbe05df3143d
Robert Marquez <legitem2023@gmail.com>
2026-02-16 00:55:28 +0800

    Update FBXViewer.tsx

--- src/app/components/FBXViewer.tsx
+++ src/app/components/FBXViewer.tsx
@@ -200,13 +200,13 @@ export default function FBXViewer({ modelPath = '/City/City.FBX' }: FBXViewerPro
         console.log(`Applying scale factor: ${scale} (from ${maxDim} to ${targetSize})`);
         
         // Apply scale
-        object.scale.set(scale * 2, scale * 2, scale * 2);
+        object.scale.set(scale * 5, scale * 5, scale * 5);
         
         object.rotation.x = -(90 * Math.PI / 180);  // Formula: degrees * PI/180
         // Center the model
         object.position.set(
           -center.x * scale,
-          5,
+          10,
           -center.z * scale
         );
         
