a0d155a26aab193245d1b16d54ab53faafc70303
Robert Marquez <legitem2023@gmail.com>
2026-02-16 01:30:11 +0800

    Update FBXViewer.tsx

--- src/app/components/FBXViewer.tsx
+++ src/app/components/FBXViewer.tsx
@@ -184,7 +184,7 @@ export default function FBXViewer({ modelPath = '/City/City.FBX' }: FBXViewerPro
       });
       const plane = new THREE.Mesh(planeGeometry, planeMaterial);
       plane.rotation.x = Math.PI / 2;
-      plane.position.y = 0;
+      plane.position.y = 5;
       plane.receiveShadow = true;
       scene.add(plane);
     };
