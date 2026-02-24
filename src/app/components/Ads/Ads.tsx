0e2a098e8472beb5bd9dbf2a610d1250e41ef5d2
Robert Marquez <legitem2023@gmail.com>
2026-02-24 08:47:38 +0800

    Update Ads.tsx

--- src/app/components/Ads/Ads.tsx
+++ src/app/components/Ads/Ads.tsx
@@ -2,9 +2,10 @@
 import Marquee from '../ui/Marquee';
 import AdDropdown from './AdDropdown';
 import CountdownAnalog from './CountdownAnalog';
-const Ads:React.FC = () => {
 
-  const Dropdown = (<CountdownAnalog/>)//(<Marquee text="Spotted! The perfect place for your products call now 09153392813" fontSize="35px" speed={10} />)
+const Ads:React.FC = () => {
+  const targetDate = new Date().getTime() + 24 * 3600 * 1000 + 5000;
+  const Dropdown = (<CountdownAnalog targetDate={targetDate}/>)//(<Marquee text="Spotted! The perfect place for your products call now 09153392813" fontSize="35px" speed={10} />)
   return (
      <AdDropdown content={Dropdown}/>
   )
