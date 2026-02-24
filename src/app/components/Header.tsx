6bffce0f8fd1d8af7dfcc516892553ee31405c7e
Robert Marquez <legitem2023@gmail.com>
2026-02-24 22:14:09 +0800

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -539,12 +539,7 @@ const Header: React.FC = () => {
 
   return (
     <div>
-      <div className="relative p-2 aspect-[4/1] sm:aspect-[9/1]"
-        style={{
-                 background: 'linear-gradient(135deg, rgba(255,255,255,0.9) 0%, rgba(200,180,255,0.5) 100%)',
-                 backdropFilter: 'blur(2px)',
-                 WebkitBackdropFilter: 'blur(2px)'
-          }}>
+      <div className="relative p-2 aspect-[4/1] sm:aspect-[9/1]">
         {/*<AnimatedCrowd/>*/}
         {/*<FBXViewer modelPath="/City/City.FBX" />*/}
         <div className="z-20 flex items-center justify-between p-2 h-[100%] w-[100%]">
