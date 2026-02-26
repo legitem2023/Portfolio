a8a693c96dc7c34e4181ef8b8882552999aacffc
Robert Marquez <legitem2023@gmail.com>
2026-02-26 20:15:58 +0800

    Update AdDropdown.tsx

--- src/app/components/Ads/AdDropdown.tsx
+++ src/app/components/Ads/AdDropdown.tsx
@@ -49,7 +49,7 @@ export default function AdDropdown({ content }: AdDropdownProps) {
   return (
     <div style={containerStyle}>
       <button style={buttonStyle} onClick={() => setIsOpen(!isOpen)}>
-        Ads
+        Countdown To Start
         <span style={iconStyle}>▼</span>
       </button>
       {isOpen && <div style={contentStyle}>{content}</div>}
