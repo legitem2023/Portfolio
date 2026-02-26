b1538d5d6b049ec0d3076b5bd1198bcdd20b36ab
Robert Marquez <legitem2023@gmail.com>
2026-02-26 23:36:00 +0800

    Update OrderSummary.tsx

--- src/app/components/Cart/OrderSummary.tsx
+++ src/app/components/Cart/OrderSummary.tsx
@@ -41,7 +41,7 @@ const OrderSummary = ({
     );
   }
  console.log("Pickup",cartItems);
- console.log("DropOff",addresses);
+ console.log("DropOff",addresses.filter((item:any) => item.isDefault===true));
   return (
     <div className="bg-white rounded">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 md:py-8">
