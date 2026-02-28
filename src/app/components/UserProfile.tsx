bfe2e7219bf419f226ef61ec4aae926949083cf4
Robert Marquez <legitem2023@gmail.com>
2026-02-28 22:53:38 +0800

    Update UserProfile.tsx

--- src/app/components/UserProfile.tsx
+++ src/app/components/UserProfile.tsx
@@ -75,6 +75,7 @@ const UserProfile = ({ userId }: { userId: string }) => {
           tabsConfig={[
             { id: 'posts', label: 'Posts', icon: 'user' },
             { id: 'address', label: 'Addresses', icon: 'location' }
+            { id: 'wishlist', label: 'Wishlist', icon: 'location' }
           ]}
         />
       </div>
