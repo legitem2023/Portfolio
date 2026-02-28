6a72905698abe7122446c1cc1d78eac7f5f2e671
Robert Marquez <legitem2023@gmail.com>
2026-03-01 00:17:55 +0800

    Update ProfileTabs.tsx

--- src/app/components/ProfileTabs.tsx
+++ src/app/components/ProfileTabs.tsx
@@ -24,7 +24,7 @@ const ProfileTabs = ({ activeTab, onTabChange, tabsConfig }: ProfileTabsProps) =
             <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
           </svg>
         );
-      case 'product':
+      case 'tags':
         return (
           <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
             <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
