fef5d471817a78160ead78cf978dad1a7600e209
Robert Marquez <legitem2023@gmail.com>
2026-03-01 11:50:48 +0800

    Update resolvers.ts

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1547,7 +1547,7 @@ unreadNotificationCount: async (_:any, { userId }:any, context:any) => {
           addresses: true,
           wishlist:{
             include: {
-              products:true
+              product:true
             }
           },
           products: {
