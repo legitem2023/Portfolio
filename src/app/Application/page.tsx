3784b05722fff649beaab66034cd918fd859e122
Robert Marquez <legitem2023@gmail.com>
2026-03-02 14:53:57 +0800

    Create page.tsx

---
+++ src/app/Application/page.tsx
@@ -0,0 +1,8 @@
+// app/Application/VendorApplicationForm.tsx
+'use client'
+import React, { useState, useEffect } from 'react';
+import VendorApplicationForm from '../components/VendorApplicationForm';
+
+export default function ApplicationPage() {
+  return VendorApplicationForm/>;
+}
