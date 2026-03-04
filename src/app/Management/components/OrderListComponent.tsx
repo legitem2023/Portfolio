8c33c41881049c725372fc19be57684059da4e97
Robert Marquez <legitem2023@gmail.com>
2026-03-05 02:10:33 +0800

    Update OrderListComponent.tsx

--- src/app/Management/components/OrderListComponent.tsx
+++ src/app/Management/components/OrderListComponent.tsx
@@ -55,6 +55,7 @@ const ORDER_LIST_QUERY = gql`
           id
           orderId
           supplierId
+          riderId
           quantity
           price
           status
@@ -135,6 +136,7 @@ interface OrderItem {
   id: string;
   orderId?: string;
   supplierId?: string;
+  riderId?: string;
   quantity: number;
   price: number;
   status?: OrderStatus;
