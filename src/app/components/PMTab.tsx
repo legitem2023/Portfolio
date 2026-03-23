f93211ce5ca84365bbaffcee67713a7c00c0b162
Robert Marquez <legitem2023@gmail.com>
2026-03-23 17:21:42 +0800

    Update PMTab.tsx

--- src/app/components/PMTab.tsx
+++ src/app/components/PMTab.tsx
@@ -824,6 +824,7 @@ const PMTab = ({ UserId }: { UserId: string }) => {
             <div className="flex-1 flex flex-col h-full bg-white relative">
               {/* Chat Header - Same gradient style without logo */}
               {selectedUser ? (
+              <div className="flex-shrink-0">
                 <div className="relative p-0 aspect-[4/1] sm:aspect-[9/1] bg-[linear-gradient(135deg,rgba(255,255,255,0.9)_0%,rgba(200,180,255,0.5)_100%)] flex-shrink-0">
                   <div className="z-20 flex items-center justify-between p-2 h-[100%] w-[100%]">
                     <div className="flex items-center space-x-3">
@@ -870,6 +871,7 @@ const PMTab = ({ UserId }: { UserId: string }) => {
                     </div>
                   </div>
                 </div>
+              </div>
               ) : (
             <div className="flex-shrink-0">
               <div className="relative p-0 aspect-[4/1] sm:aspect-[9/1] bg-[linear-gradient(135deg,rgba(255,255,255,0.9)_0%,rgba(200,180,255,0.5)_100%)]">
