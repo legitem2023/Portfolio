ff03012b95e66c8358d4800027bc03566d6f0d06
Robert Marquez <legitem2023@gmail.com>
2026-03-27 12:32:18 +0800

    Update ProductsTab.tsx

--- src/app/components/ProductsTab.tsx
+++ src/app/components/ProductsTab.tsx
@@ -357,8 +357,7 @@ const ProductsTab: React.FC = () => {
               className="border border-gray-300 rounded-md px-3 py-2 text-sm w-full"  
               value={sortBy}  
               onChange={handleSortChange}  
-            >  
-              <option>Sort by: Featured</option>  
+            >
               <option>Sort by: Newest</option>  
               <option>Sort by: Price: Low to High</option>  
               <option>Sort by: Price: High to Low</option>  
