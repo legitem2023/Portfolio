d0d3a4f133d5d0e95fe5babcd7367077b5d0256d
Robert Marquez <legitem2023@gmail.com>
2026-03-29 08:11:00 +0800

    Update page.tsx

--- src/app/Management/page.tsx
+++ src/app/Management/page.tsx
@@ -57,7 +57,6 @@ export default function ManagementDashboard() {
 
   const [newCategory, setNewCategory] = useState<NewCategory>({
     name: "",
-    image: "",
     description: "",
     parentId: "",
     isActive: true
