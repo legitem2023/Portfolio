f1c905a9da70452e474dcbfea134802df359c477
Robert Marquez <legitem2023@gmail.com>
2026-04-06 19:30:00 +0800

    Update server.ts

--- src/app/lib/pusher/server.ts
+++ src/app/lib/pusher/server.ts
@@ -2,9 +2,9 @@
 import Pusher from 'pusher';
 
 export const pusherServer = new Pusher({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.PUSHER_KEY!,
-  secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.PUSHER_CLUSTER!,
-  useTLS: true,
+  appId: "2137516",
+  key: "89df54e492d888d001ed",
+  secret: "a684061715e3e3710b74",
+  cluster: "ap1",
+  useTLS: true
 });
