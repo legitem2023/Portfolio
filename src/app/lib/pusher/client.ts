43ba15e7e378331f33fd350a4a14fc81fdb7d600
Robert Marquez <legitem2023@gmail.com>
2026-04-06 19:34:48 +0800

    Create client.ts

---
+++ src/app/lib/pusher/client.ts
@@ -0,0 +1,13 @@
+// lib/pusher/client.ts
+import Pusher from 'pusher-js';
+
+let pusherClient: Pusher | null = null;
+
+export const getPusherClient = () => {
+  if (!pusherClient && typeof window !== 'undefined') {
+    pusherClient = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
+      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+    });
+  }
+  return pusherClient;
+};
