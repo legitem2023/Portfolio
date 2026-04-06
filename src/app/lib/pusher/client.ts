dddf7fe676db0433323f3568847fcc503af7a43a
Robert Marquez <legitem2023@gmail.com>
2026-04-06 21:21:37 +0800

    Update client.ts

--- src/app/lib/pusher/client.ts
+++ src/app/lib/pusher/client.ts
@@ -1,13 +1,15 @@
-// lib/pusher/client.ts
+// src/app/lib/pusher/client.ts
+
 import Pusher from 'pusher';
 
 let pusherClient: Pusher | null = null;
 
 export const getPusherClient = () => {
   if (!pusherClient && typeof window !== 'undefined') {
-    pusherClient = new Pusher("89df54e492d888d001ed", {
-      cluster: "ap1",
+    pusherClient = new Pusher({
+      appKey: "89df54e492d888d001ed",
+      cluster: "ap1"
     });
   }
   return pusherClient;
-};
+}
