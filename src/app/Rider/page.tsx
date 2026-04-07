5f2e78d1ad9dfb920857e6394f80b23b06356783
Robert Marquez <legitem2023@gmail.com>
2026-04-07 18:58:03 +0800

    Update page.tsx

--- src/app/Rider/page.tsx
+++ src/app/Rider/page.tsx
@@ -37,7 +37,7 @@ const MapTab = dynamic(() => import('./components/MapTab'), {
 
 // ONLY the mutation - no type definitions here!
 const LOCATION_TRACKING_MUTATION = gql`
-  mutation LocationTracking($input: LocationInput!) {
+  mutation LocationTracking($input: LocationTrackingInput) {
     locationTracking(input: $input) {
       userID
       latitude
