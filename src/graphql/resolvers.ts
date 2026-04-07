db5e9747e7c467978d751b714174c1757dfa09e4
Robert Marquez <legitem2023@gmail.com>
2026-04-07 11:06:00 +0800

    Update resolvers.ts

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -14,16 +14,6 @@ import {
 } from './Types/graphql.js';
 
 
-import Pusher from 'pusher';
-
-export const pusherServer = new Pusher({
-  appId: "2137516",
-  key: "89df54e492d888d001ed",
-  secret: "a684061715e3e3710b74",
-  cluster: "ap1",
-  useTLS: true
-});
-
 const prisma = new PrismaClient();
 
 // Add date-fns imports for sales analytics
