30b04295ef7c3346682f9af5da00851c0dfe0332
Robert Marquez <legitem2023@gmail.com>
2026-04-08 13:08:53 +0800

    Update AppWithTabs.tsx

--- src/app/Separator/AppWithTabs.tsx
+++ src/app/Separator/AppWithTabs.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react';
 import VectorizeBlurredImage from './VectorizeBlurredImage';
 import SimpleSilkscreenSeparator from './SimpleSilkscreenSeparator';
