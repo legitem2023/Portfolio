ff91db36cd6b8d07a5694b25a4069a9fddd0e6b5
Robert Marquez <legitem2023@gmail.com>
2026-04-11 21:06:44 +0800

    Change div class for Login page background

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -243,7 +243,7 @@ export default function LuxuryLogin() {
         <meta name="description" content="Login to VendorCity Account" />
       </Head>
       
-      <div className="min-h-screen bg-gradient-to-b from-indigo-50 to-violet-50 p-0">
+      <div className="bg-gradient-to-b from-indigo-50 to-violet-50 p-0">
         <Header/>
         <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div className="max-w-2xl w-full space-y-8 bg-white p-10 rounded-xl shadow-2xl border border-indigo-100">
