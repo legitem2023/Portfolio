27c90e9e53f6d7875fbf2648d5ef4de0a56c1cb0
Robert Marquez <legitem2023@gmail.com>
2026-04-18 22:09:25 +0800

    Add RiderSignupForm to ApplicationPage

--- src/app/Application/Rider/page.tsx
+++ src/app/Application/Rider/page.tsx
@@ -1,1 +1,8 @@
+// app/Application/RiderSignupForm.tsx
+'use client'
+import React, { useState, useEffect } from 'react';
+import RiderSignupForm from '../../components/RiderSignupForm';
 
+export default function ApplicationPage() {
+  return <RiderSignupForm/>;
+}
