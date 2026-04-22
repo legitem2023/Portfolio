180d8c4185de3b66c885c4081a15133ff2d8985d
Robert Marquez <legitem2023@gmail.com>
2026-04-22 14:07:56 +0800

    Comment out upload overlay in MobileProductCard
    Comment out the upload overlay div in MobileProductCard.

--- src/app/Management/components/Products/MobileProductCard.tsx
+++ src/app/Management/components/Products/MobileProductCard.tsx
@@ -123,7 +123,7 @@ export default function MobileProductCard({
               )}
 
               {/* Upload Overlay */}
-              <div className="absolute inset-0 bg-black/60 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity">
+              {/* <div className="absolute inset-0 bg-black/60 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity">
                 <label className="cursor-pointer p-1">
                   <svg className="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
@@ -136,7 +136,7 @@ export default function MobileProductCard({
                     disabled={isUploading}
                   />
                 </label>
-              </div>
+              </div>*/}
 
               {isUploading && (
                 <div className="absolute inset-0 flex items-center justify-center bg-black/70">
