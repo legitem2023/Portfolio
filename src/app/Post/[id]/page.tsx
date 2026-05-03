a47d18e94cdc9beb37099649b97969cef9d34d62
Robert Marquez <legitem2023@gmail.com>
2026-05-04 00:09:41 +0800

    Create user profile page with role fetching

---
+++ src/app/Post/[id]/page.tsx
@@ -0,0 +1,47 @@
+// app/profile/[id]/page.tsx
+'use client'
+import React, { useState, useEffect } from 'react';
+import PMTab from '../../components/PMTab';
+import OutFolderTabs from '../../components/OutFolderTabs';
+import Header from '../../components/Header';
+import { decryptToken } from '../../../../utils/decryptToken';
+
+export default function Page() {
+  const [userId, setUserId] = useState("");
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState("");
+   
+  useEffect(() => {
+    const getRole = async () => {
+      try {
+        const response = await fetch('/api/protected', {
+          credentials: 'include'
+        });
+        
+        if (response.status === 401) {
+          throw new Error('Unauthorized');
+        }
+        
+        const data = await response.json();
+        const token = data?.user;
+        const secret = process.env.NEXT_PUBLIC_JWT_SECRET || "QeTh7m3zP0sVrYkLmXw93BtN6uFhLpAz";
+
+        const payload = await decryptToken(token, secret.toString());
+        setUserId(payload.userId);
+        setName(payload.name);
+        setAvatar(payload.image || "/NoImage.webp");
+      } catch (err) {
+        console.error('Error getting role:', err);
+      }
+    };
+    getRole();
+  }, []);
+  return (
+    <>
+      <div className="h-[100vh] bg-gradient-to-b from-indigo-50 to-violet-50 p-0">
+        <OutFolderTabs/>
+        <PMTab UserId={userId} />
+      </div> 
+    </>
+  )    
+}
