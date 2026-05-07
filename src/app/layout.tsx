f93b34a4639b04c2df8cf5c32f591435c510d836
Robert Marquez <legitem2023@gmail.com>
2026-05-07 12:48:44 +0800

    Refactor layout to use ClientLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
-"use client";
 import { metadata as baseMetadata, viewport as baseViewport } from './components/Seo/Seo';
-import ReduxWrapper from "./components/ApolloProvider/ReduxWrapper"; 
-import PWAInitializer from "./components/PWAInitializer";
-import InstallPWAButton from './components/InstallPWAButton';
-
-import { ApolloWrapper } from './components/ApolloWrapper';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import ClientLayout from './ClientLayout';
 import { Inter } from "next/font/google";
-import LoadEruda from "./LoadEruda";
 import "./globals.css";
 import "./styles/messaging.css";
-
-// ✅ Import SessionProvider from NextAuth
-import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,16 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <LoadEruda/>
-        <div className="BackGroundImage"></div>
-        <PWAInitializer/>
-        {/* ✅ Wrap with SessionProvider */}
-        <SessionProvider>
-          <ReduxWrapper>
-            <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop />
-            <ApolloWrapper>{children}</ApolloWrapper>
-          </ReduxWrapper>
-        </SessionProvider>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
