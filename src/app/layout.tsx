d5843d5e0f77ff967e655e4d023b2972a13672b4
Robert Marquez <legitem2023@gmail.com>
2026-05-07 12:24:58 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         <LoadEruda/>
         <div className="BackGroundImage"></div>
         <PWAInitializer/>
-        <InstallPWAButton/>
         {/* ✅ Wrap with SessionProvider */}
         <SessionProvider>
           <ReduxWrapper>
