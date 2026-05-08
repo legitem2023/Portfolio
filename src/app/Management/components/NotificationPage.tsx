e5f2459fd1e61a824275661e51be87cef8e44dc7
Robert Marquez <legitem2023@gmail.com>
2026-05-08 10:10:08 +0800

    Update NotificationPage.tsx

--- src/app/Management/components/NotificationPage.tsx
+++ src/app/Management/components/NotificationPage.tsx
@@ -26,7 +26,7 @@ export default function NotificationPage({ UserId }: { UserId: string }) {
         error={error?.message}
         onNotificationClick={(notification) => {
           // Handle click
-          console.log(notifocation);
+          console.log(notification);
           //router.push(notification.link || '/');
         }}
         onMarkAsRead={(id) => markAsRead({ variables: { id } })}
