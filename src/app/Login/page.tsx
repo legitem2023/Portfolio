9be53fd1e5709ab97f4ade83cd193851397cc4ee
Robert Marquez <legitem2023@gmail.com>
2026-05-09 19:11:23 +0800

    Fix HTML entity for apostrophe in Login page

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -464,7 +464,7 @@ export default function LuxuryLogin() {
               
               <div className="text-center">
                 <p className="text-sm text-gray-600">
-                  Don't have an account?{' '}
+                  Don&apos;t have an account?{' '}
                   <Link href="/Signup" className="font-medium text-indigo-600 hover:text-indigo-500">
                     Sign up
                   </Link>
