b94d3bcedb1df271181a083be9df54bce234ce1d
Robert Marquez <legitem2023@gmail.com>
2026-05-31 17:19:00 +0800

    Update ClientLayout.tsx

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -6,6 +6,7 @@ import InstallPWAButton from './components/InstallPWAButton';
 import { ApolloWrapper } from './components/ApolloWrapper';
 import { ToastContainer } from 'react-toastify';
 import { NotificationManager } from './components/NotificationManager';
+import ServiceWorkerRegister from './components/ServiceWorkerRegister';
 import 'react-toastify/dist/ReactToastify.css';
 import LoadEruda from "./LoadEruda";
 import { SessionProvider } from "next-auth/react";
@@ -21,6 +22,7 @@ export default function ClientLayout({
       <div className="BackGroundImage"></div>
       <PWAInitializer />
       <NotificationManager/>
+      <ServiceWorkerRegister/>
       <SessionProvider>
         <ReduxWrapper>
           <ToastContainer 
