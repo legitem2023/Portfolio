43aee98efea15e7e9fc27a9282c6b3d92c9a0003
Robert Marquez <legitem2023@gmail.com>
2026-06-14 11:07:09 +0800

    Update activeIndexSlice.ts

--- Redux/activeIndexSlice.ts
+++ Redux/activeIndexSlice.ts
@@ -1,31 +1,3 @@
-// store/features/activeIndexSlice.ts
-/*import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-interface ActiveIndexState {
-  value: number;
-}
-
-const initialState: ActiveIndexState = {
-  value: 1,
-};
-
-export const activeIndexSlice = createSlice({
-  name: 'activeIndex',
-  initialState,
-  reducers: {
-    setActiveIndex: (state, action: PayloadAction<number>) => {
-      state.value = action.payload;
-    },
-    resetActiveIndex: (state) => {
-      state.value = 1;
-    },
-  },
-});
-
-export const { setActiveIndex, resetActiveIndex } = activeIndexSlice.actions;
-export default activeIndexSlice.reducer;
-*/
-// store/features/activeIndexSlice.ts
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface ActiveIndexState {
